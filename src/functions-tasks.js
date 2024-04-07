5f689fe77f2fd212ed656e43032f5967a0e214ac
andreiananko <110332587+andreiananko@users.noreply.github.com>
2024-04-07 16:47:10 +0300

    feat: added function getArgumentsCount(funcs)

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -50,8 +50,9 @@ function getFunctionBody(func) {
  *  ]) => [0, 1, 2]
  *
  */
-function getArgumentsCount(/* funcs */) {
-  throw new Error('Not implemented');
+function getArgumentsCount(funcs) {
+  const result = funcs.map((func) => func.length);
+  return result;
 }
 
 /**
